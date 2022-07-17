14389c3e6abf52a7d3e65a3d4eb9340c938110b4
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-07-17 09:00:39 +0300

    adding hook useLocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { nanoid } from 'nanoid';
@@ -7,26 +7,16 @@ import { ContactForm } from 'components/ContactForm';
 import { Filter } from 'components/Filter';
 import { Box } from 'components/Box';
 import { CONTACTS_KEY } from 'constants/constants';
+import { useLocalStorage } from 'hooks/useLocalStorage';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
+  const [contacts, setContacts] = useLocalStorage(CONTACTS_KEY, [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ]);
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const data = localStorage.getItem(CONTACTS_KEY);
-    if (data) {
-      setContacts(JSON.parse(data));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem(CONTACTS_KEY, JSON.stringify(contacts));
-  }, [contacts]);
 
   const dataValidation = data =>
     contacts.find(
