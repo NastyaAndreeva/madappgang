5848871b2b56cfc16a43ee66d0669e8393cb19e9
NastyaAndreeva <andreeva.anastasiya.v@gmail.com>
2022-08-26 19:29:45 +0300

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -38,3 +38,51 @@ export const App = () => {
     </>
   );
 };
+
+// function isIsogram(str) {
+//   const normalizedArr = str.toLowerCase().split('').sort();
+//   const uniqueElements = normalizedArr.filter(
+//     (el, index, array) => array.indexOf(el) === index
+//   );
+//   return uniqueElements.length === normalizedArr.length ? true : false;
+// }
+
+// function isIsogram(str) {
+//   return new Set(str.toUpperCase()).size === str.length;
+// }
+
+// isIsogram('Dermatoglyphics');
+
+// isIsogram('aba');
+// function updateLight(current) {
+//   const state = ['green', 'yellow', 'red'];
+//   for (let i = 0; i < 3; i++) {
+//     if (current === state[i]) {
+//       return state[i + 1];
+//     }
+//   }
+// }
+
+// console.log(updateLight('green'));
+
+// const updateLight = current =>
+//   ({
+//     green: 'yellow',
+//     yellow: 'red',
+//     red: 'green',
+//   }[current]);
+
+// console.log(updateLight('green'));
+
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i += 1) {
+    let current = numbers[i];
+    for (let j = 1; j < numbers.length; j += 1) {
+      if (current + numbers[j] === target) {
+        return i === j ? [i] : [i, j];
+      }
+    }
+  }
+}
+
+console.log(twoSum([2, 2, 3], 4)); // returns [0, 2] or [2, 0]
